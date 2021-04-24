f966c4ed42b2f66228309cb9b8bb2b70c6bb7e74
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-24 05:55:25 +0000

    [Update]Rubocop

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,28 +1,28 @@
 class TodolistsController < ApplicationController
   def new
-    @list=List.new
+    @list = List.new
   end
 
   def create
-    list=List.new(list_params)
+    list = List.new(list_params)
     list.save
     redirect_to todolist_path(list.id)
   end
 
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def update
-    list=List.find(params[:id])
+    list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
@@ -34,9 +34,8 @@ class TodolistsController < ApplicationController
   end
 
   private
+
   def list_params
-    params.require(:list).permit(:title,:body,:image)
+    params.require(:list).permit(:title, :body, :image)
   end
-
-
 end
